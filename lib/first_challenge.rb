4132616b33826322170e7596d5524ebfad379687
Rahul Mody <rmody3@gmail.com>
2017-01-30 21:16:35 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,14 @@ def first_challenge
   }
 
   #your code here
-
+  contacts["Freddy Mercury"].each do |key, data|
+    if key == :favorite_icecream_flavors
+      data.delete_if  do |ice_cream| ice_cream == "strawberry"
+      end
+    end
+  end
+    
+    
 
   #remember to return your newly altered contacts hash!
   contacts
